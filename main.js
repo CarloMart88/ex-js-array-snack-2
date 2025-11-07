1dfb224593ba040cc501881bdec58197d6ab1c6a
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-07 16:26:04 +0100

    correct method

--- main.js
+++ main.js
@@ -191,7 +191,7 @@ Crea un array (booksByPrice) con gli elementi di books ordinati in base al prezz
 Ordina l’array booksByPricein base alla disponibilità (prima quelli disponibili), senza creare un nuovo array.*/
 
 //Crea una variabile booleana (areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
-const areThereAvailableBooks = books.filter(b => b.available)
+const areThereAvailableBooks = books.some(b => b.available)
 
 console.log(areThereAvailableBooks)
 
